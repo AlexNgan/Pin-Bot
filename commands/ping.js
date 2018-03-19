874515f15ab56d9fd79b97a65c2102e1a27a3dd3
Mortyyy <ngang@bxscience.edu>
2018-03-19 18:49:42 -0400

    god....

--- commands/ping.js
+++ commands/ping.js
@@ -2,3 +2,17 @@
 exports.run = (bot, message, args, level) => {
     message.channel.send("pong! Your ping to my server is " + Math.round(bot.ping) + " milliseconds.").catch(console.error);
 };
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "ping",
+  category: "Miscelaneous",
+  description: "It... like... pings. Then Pongs. And it\"s not Ping Pong.",
+  usage: "ping"
+};
