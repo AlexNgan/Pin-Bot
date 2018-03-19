a76d7950cf3fd50326d96e18d16aada2a8e5c560
Mortyyy <ngang@bxscience.edu>
2018-03-19 17:35:41 -0400

    Enmap.

--- index.js
+++ index.js
@@ -11,14 +11,25 @@ const readdir = promisify(require("fs").readdir);
 //Stores shit.
 const Enmap = require("enmap");
 const EnmapLevel = require("enmap-level");
-const tableSource = new EnmapLevel({name: "table"});
-bot.table = new Enmap({provider: tableSource});
 
 const config = require("./config.json");  //Allows config details to be stored secretly
 require("./modules/functions.js")(bot); // Useful functions that we'll use throughout the bot, like logs and elevation features.
 //const fs = require("fs");
-bot.commands = new Discord.Collection();
-bot.aliases = new Discord.Collection();
+
+// Aliases and commands are put in collections where they can be read from, catalogued, listed, etc.
+bot.commands = new Enmap();
+bot.aliases = new Enmap();
+
+//Essentially saves a collection to disk.
+bot.settings = new Enmap({provider: new EnmapLevel({name: "settings"})});
+
+const defaultSettings = {
+  prefix: "-",
+  //modLogChannel: "abandonment",
+  modRole: "Mod Birb",
+  adminRole: "Admin Birb",
+  //welcomeMessage: "Say hello to {{user}}, everyone! We all need a warm welcome sometimes :D"
+}
 
     // super-secret recipe to call events with all their proper arguments *after* the `client` var.
     //bot.on(eventName, (...args) => eventFunction.run(bot, ...args));
