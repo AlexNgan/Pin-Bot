839b7e4cc534a77b15aa5a0c3934ac72ca96024f
Mortyyy <ngang@bxscience.edu>
2018-03-19 18:58:36 -0400

    hi

--- events/Hof.js
+++ events/Hof.js
@@ -1,1 +1,10 @@
 //idk
+module.exports = async bot => {
+  // Why await here? Because the ready event isn't actually ready, sometimes
+  // guild information will come in *after* ready. 1s is plenty, generally,
+  // for all of them to be loaded.
+  await bot.wait(1000);
+
+  // Both `wait` and `client.log` are in `./modules/functions`.
+  bot.logger.log(`[READY] ${bott.user.tag}, ready to serve ${bot.users.size} users in ${bot.guilds.size} servers.`, "ready");
+};
